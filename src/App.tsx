9b7290423f556d922e8021e72a893a22e2596c22
Zhenkaaf <zhenkaaf@gmail.com>
2023-05-22 17:10:13 +0300

    first

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import TodoItem from './components/TodoItem';
+import NewTodoForm from './components/NewTodoForm';
+import {Todo} from './types';
+import TodoList from './components/TodoList';
+
+
+type ITodo = {
+  id: string,
+  title: string,
+  completed: boolean,
+}
 
 function App() {
+
+const [text, setText] = useState('');
+const [todos, setTodos] = useState<Todo[]>([]);
+const [] = useState<ITodo | null>(null);
+
+const handleInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+  setText(event.target.value);
+}
+const addTodo = (text: string /* -useRef */) => {
+  const newTodo: Todo = {
+    id: new Date().toString(),
+    title: text,
+    completed: false,
+  }
+  setTodos([newTodo, ...todos]);
+  //setText(''); -useref
+}
+
+const toggleTodo = (id: Todo['id']) => {
+  setTodos(todos.map(todo => {
+    if(todo.id !== id) return todo;
+    return {
+      ...todo,
+      completed: !todo.completed,
+    }
+  }))
+}
+const removeTodo = (id: Todo['id']) => {
+  setTodos(todos.filter(todo => todo.id !== id))
+}
+
+/* useEffect(() => {
+  fetch('https://jsonplaceholder.typicode.com/todos')
+  .then(res => res.json())
+  .then((data: Todo[]) => {
+    setTodos(data);
+  })
+}, []); */
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NewTodoForm /* value={text} onChange={handleInput} /-useRef */ handleClick={addTodo}/>
+      <TodoItem toggleTodo={toggleTodo} removeTodo={removeTodo} id='112' title='first todo' completed={false} style={{border: '1px solid green'}} />
+      <TodoList list={todos} toggleTodo={toggleTodo} removeTodo={removeTodo}/>
     </div>
   );
 }
---
+++ src/components/TodoItem.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import {Todo} from '../types';
+
+interface TodoItemProps extends Todo {
+    /* id: string,
+    title: string,
+    completed: boolean, приходит из extends Todo*/
+   /*  children: React.ReactNode, если обернуть эоементом TodoItem другой элемент, он попадет в чилдрен */ 
+    style?: React.CSSProperties,
+    toggleTodo: (id: Todo['id']) => void,
+    removeTodo: (id: Todo['id']) => void,
+}
+
+/* const TodoItem: React.FC<TodoItemProps> = ({id, title, completed}) => { */
+const TodoItem = ({id, toggleTodo, removeTodo, title, completed, /* children */ style={}}: TodoItemProps) => {
+    return (
+        <li style={{color: 'red', backgroundColor: 'white', ...style }}>
+            <input type="checkbox" checked={completed} onChange={() => toggleTodo(id)} />
+            <span>{title}</span>
+            <span onClick={() => removeTodo(id)}>&times;</span>
+          {/*   {children} */}
+        </li>
+    )
+}
+export default TodoItem;
